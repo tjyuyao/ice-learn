bbae328abe0a6ae0222baed7695bbbebd7c50fb0
Yuyao Huang <yycv.simon@protonmail.com>
2022-01-19 13:53:17 +0800

    [Tiny] add poetry run prefix.

--- docs/build_references.sh
+++ docs/build_references.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Note: run this script in REPOSITORY_ROOT.
-lazydocs \
+poetry run lazydocs \
     --output-path="./docs/references/" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/tjyuyao/ice-learn/blob/main/" \
