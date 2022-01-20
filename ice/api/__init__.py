4e67959dd4b5b862c003344ed8396a048e8696eb
Yuyao Huang <yycv.simon@protonmail.com>
2022-01-20 08:01:49 +0800

    [Feat] PyCUDA integration.

--- ice/api/__init__.py
+++ ice/api/__init__.py
@@ -1,3 +1,4 @@
 from ice.llutil.config import configurable, make_configurable, is_configurable, clone, freeze
 from ice.llutil import multiprocessing
 from ice.llutil.multiprocessing import called_from_main
+from ice.llutil.cuda import CUDAModule
