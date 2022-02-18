2006255986fdd28cf6ea87ee4b1cf44555f59201
Yuyao Huang <yycv.simon@protonmail.com>
2022-02-18 11:25:56 +0800

    [Bugs] show all trace when DEBUG_ICE is not set

--- ice/llutil/shadow_tb.py
+++ ice/llutil/shadow_tb.py
@@ -1,11 +1,6 @@
 import os
 
 if "DEBUG_ICE" not in os.environ:
-
-    import sys
-
-    from traceback import extract_tb, format_list, format_exception_only
-
     SHADOW_PATTERNS = (
         'torch/nn/modules/',
         'pickle.py',
@@ -15,36 +10,43 @@ if "DEBUG_ICE" not in os.environ:
         'ice/llutil/',
         'ice/core/',
         )
+else:
+    SHADOW_PATTERNS = ()
 
-    def _check_file(name):
-        if not name: return False
-        for pattern in SHADOW_PATTERNS:
-            if name.find(pattern) != -1: return False
-        return True
+import sys
 
-
-    def shadow(etype, evalue, tb):
-        extracted_tb = extract_tb(tb)
-        show = [fs for fs in extracted_tb[:-1] if _check_file(fs.filename)]
-        show.append(extracted_tb[-1])
-        fmt = format_list(show) + format_exception_only(etype, evalue)
-        print(''.join(fmt), end='', file=sys.stderr)
+from traceback import extract_tb, format_list, format_exception_only
 
 
-    sys.excepthook = shadow
+def _check_file(name):
+    if not name: return False
+    for pattern in SHADOW_PATTERNS:
+        if name.find(pattern) != -1: return False
+    return True
 
 
-    try:
-        ipython = get_ipython()
+def shadow(etype, evalue, tb):
+    extracted_tb = extract_tb(tb)
+    show = [fs for fs in extracted_tb[:-1] if _check_file(fs.filename)]
+    show.append(extracted_tb[-1])
+    fmt = format_list(show) + format_exception_only(etype, evalue)
+    print(''.join(fmt), end='', file=sys.stderr)
 
-        def shadow_ipython(etype, evalue, stb):
-            stb = [s for s in stb if _check_file(s)]
-            val = '\n'.join(stb)
-            try:
-                print(val)
-            except UnicodeEncodeError:
-                print(val.encode("utf-8", "backslashreplace").decode())
-        
-        ipython._showtraceback = shadow_ipython
-        
-    except: pass
+
+sys.excepthook = shadow
+
+
+try:
+    ipython = get_ipython()
+
+    def shadow_ipython(etype, evalue, stb):
+        stb = [s for s in stb if _check_file(s)]
+        val = '\n'.join(stb)
+        try:
+            print(val)
+        except UnicodeEncodeError:
+            print(val.encode("utf-8", "backslashreplace").decode())
+    
+    ipython._showtraceback = shadow_ipython
+    
+except: pass
