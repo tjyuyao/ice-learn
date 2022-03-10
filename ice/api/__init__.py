ae1fdd380a7f1fb93e73d33cf13d80d6e159b78b
Ling Wang <zgjshawl@gmail.com>
2022-03-10 16:49:58 +0800

    [Bug] import pycuda optionally

--- ice/api/__init__.py
+++ ice/api/__init__.py
@@ -8,7 +8,10 @@ from ice.llutil.debug import set_trace
 from ice.llutil.launcher import ElasticLauncher, get_current_launcher
 from ice.llutil.print import _print as print
 
-from ice.llutil.pycuda import CUDAModule
+try:
+    from ice.llutil.pycuda import CUDAModule
+except ImportError:
+    pass
 from ice.llutil.dictprocess import dictprocess
 from ice.llutil.multiprocessing import in_main_process
 
