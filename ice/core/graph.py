56ea99455ca834eb123338a2ae4dc7ff8df3aff4
Yuyao Huang <yycv.simon@protonmail.com>
2022-03-15 13:44:12 +0800

    [Bug] fixed using autocast and grad_acc together.

--- ice/core/graph.py
+++ ice/core/graph.py
@@ -1,6 +1,7 @@
 """contains ``Node`` and ``ExecutableGraph``."""
+from __future__ import annotations
 
-from typing import Any, Callable, Dict, List, overload
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, overload
 
 import torch
 
@@ -10,6 +11,8 @@ from ice.llutil.launcher.launcher import get_current_launcher
 from torch.cuda.amp.grad_scaler import GradScaler
 from ice.llutil.board import BoardWriter
 
+if TYPE_CHECKING:
+    from ice.core.hypergraph import Task, HyperGraph
 
 class InvalidURIError(Exception):
     """An Exception raised when valid node URI is expected."""
@@ -209,12 +212,12 @@ class GraphOutputCache:
 class ExecutableGraph:
 
     def __init__(self, hypergraph) -> None:
-        self.hypergraph = hypergraph
+        self.hypergraph: HyperGraph = hypergraph
         self.nodes:Dict[str, Node] = {}
         self.node_tags:Dict[Node, List[str]] = {}
         self.node_names:Dict[Node, str] = {}
         self.cache = GraphOutputCache(self)
-        self.task = None
+        self.task: Task = None
         self.losses_counter = 0
         self.total_loss = 0
 
@@ -264,7 +267,8 @@ class ExecutableGraph:
             self.apply("forward")
             self.apply("backward")
             self.apply("update")
-            self.grad_scaler.update()
+            if self.task.global_auto_steps % self.hypergraph.grad_acc_steps == 0:
+                self.grad_scaler.update()
         else: # eval
             self.apply("forward")
             self.apply("update")
