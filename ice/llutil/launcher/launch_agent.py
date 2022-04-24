5eaf8e0e575dc7270a746b10f5aa6e66ca3197c5
Yuyao Huang <yycv.simon@protonmail.com>
2022-04-25 04:03:53 +0800

    [close #11] compitability with torch1.11

--- ice/llutil/launcher/launch_agent.py
+++ ice/llutil/launcher/launch_agent.py
@@ -10,10 +10,11 @@ import uuid
 from dataclasses import dataclass, field
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union, cast
 
+import torch
 import torch.distributed.elastic.rendezvous.registry as rdzv_registry
 from torch.distributed.elastic import events, metrics
 from torch.distributed.elastic.agent.server.api import WorkerSpec, WorkerState
-from torch.distributed.elastic.multiprocessing import Std
+from torch.distributed.elastic.multiprocessing import SignalException, Std
 from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
 from torch.distributed.elastic.rendezvous import RendezvousParameters
 from torch.distributed.elastic.rendezvous.utils import parse_rendezvous_endpoint
@@ -107,6 +108,45 @@ def _construct_event(config: LaunchConfig) -> events.Event:
     )
 
 
+class elastic_launch:
+    """
+    Launches an torchelastic agent on the container that invoked the entrypoint.
+
+        1. Pass the ``entrypoint`` arguments as non ``kwargs`` (e.g. no named parameters)/
+           ``entrypoint`` can be a function or a command.
+        2. The return value is a map of each worker's output mapped
+           by their respective global rank.
+
+    Usage
+
+    ::
+
+    def worker_fn(foo):
+        # ...
+
+    def main():
+        # entrypoint is a function.
+        outputs = elastic_launch(LaunchConfig, worker_fn)(foo)
+        # return rank 0's output
+        return outputs[0]
+
+        # entrypoint is a command and ``script.py`` is the python module.
+        ouptuts = elestic_launch(LaunchConfig, "script.py")(args)
+        ouptuts = elestic_launch(LaunchConfig, "python")("script.py")
+    """
+
+    def __init__(
+        self,
+        config: LaunchConfig,
+        entrypoint: Union[Callable, str, None],
+    ):
+        self._config = config
+        self._entrypoint = entrypoint
+
+    def __call__(self, *args):
+        return launch_agent(self._config, self._entrypoint, list(args))
+
+
 def _get_entrypoint_name(
     entrypoint: Union[Callable, str, None], args: List[Any]
 ) -> str:
@@ -148,9 +188,8 @@ def _get_addr_and_port(
     return (master_addr, master_port)
 
 
-# pyre-fixme[56]: Pyre was not able to infer the type of the decorator
-# torch.distributed.elastic.multiprocessing.errors.record.
-def launch_agent(
+
+def launch_agent_1_10(
     config: LaunchConfig,
     entrypoint: Union[Callable, str, None],
     args: List[Any],
@@ -158,7 +197,7 @@ def launch_agent(
 ) -> Dict[int, Any]:
     if not config.run_id:
         run_id = str(uuid.uuid4().int)
-        logger.warning(f"config has no run_id, generate a new one: {run_id}")
+        logger.warning(f"config has no run_id, generated a random run_id: {run_id}")
         config.run_id = run_id
 
     entrypoint_name = _get_entrypoint_name(entrypoint, args)
@@ -189,7 +228,6 @@ def launch_agent(
     )
 
     agent = None
-    rdzv_handler = rdzv_registry.get_rendezvous_handler(rdzv_parameters)
     master_addr, master_port = _get_addr_and_port(rdzv_parameters)
     try:
         spec = WorkerSpec(
@@ -197,7 +235,7 @@ def launch_agent(
             local_world_size=config.nproc_per_node,
             entrypoint=entrypoint,
             args=tuple(args),
-            rdzv_handler=rdzv_handler,
+            rdzv_handler=rdzv_registry.get_rendezvous_handler(rdzv_parameters),
             max_restarts=config.max_restarts,
             monitor_interval=config.monitor_interval,
             redirects=config.redirects,
@@ -235,4 +273,104 @@ def launch_agent(
             events.record(_construct_event(config))
         raise
     finally:
-        rdzv_handler.shutdown()
+        spec.rdzv_handler.shutdown()
+
+
+def launch_agent_1_11(
+    config: LaunchConfig,
+    entrypoint: Union[Callable, str, None],
+    args: List[Any],
+    ice_events: Events,
+) -> Dict[int, Any]:
+    if not config.run_id:
+        run_id = str(uuid.uuid4().int)
+        logger.warning(f"config has no run_id, generated a random run_id: {run_id}")
+        config.run_id = run_id
+
+    entrypoint_name = _get_entrypoint_name(entrypoint, args)
+
+    logger.info(
+        f"Starting elastic_operator with launch configs:\n"
+        f"  entrypoint       : {entrypoint_name}\n"
+        f"  min_nodes        : {config.min_nodes}\n"
+        f"  max_nodes        : {config.max_nodes}\n"
+        f"  nproc_per_node   : {config.nproc_per_node}\n"
+        f"  run_id           : {config.run_id}\n"
+        f"  rdzv_backend     : {config.rdzv_backend}\n"
+        f"  rdzv_endpoint    : {config.rdzv_endpoint}\n"
+        f"  rdzv_configs     : {config.rdzv_configs}\n"
+        f"  max_restarts     : {config.max_restarts}\n"
+        f"  monitor_interval : {config.monitor_interval}\n"
+        f"  log_dir          : {config.log_dir}\n"
+        f"  metrics_cfg      : {config.metrics_cfg}\n"
+    )
+
+    rdzv_parameters = RendezvousParameters(
+        backend=config.rdzv_backend,
+        endpoint=config.rdzv_endpoint,
+        run_id=config.run_id,
+        min_nodes=config.min_nodes,
+        max_nodes=config.max_nodes,
+        **config.rdzv_configs,
+    )
+
+    master_addr, master_port = _get_addr_and_port(rdzv_parameters)
+
+    spec = WorkerSpec(
+        role=config.role,
+        local_world_size=config.nproc_per_node,
+        entrypoint=entrypoint,
+        args=tuple(args),
+        rdzv_handler=rdzv_registry.get_rendezvous_handler(rdzv_parameters),
+        max_restarts=config.max_restarts,
+        monitor_interval=config.monitor_interval,
+        redirects=config.redirects,
+        tee=config.tee,
+        master_addr=master_addr,
+        master_port=master_port,
+    )
+
+    agent = LocalElasticAgent(
+        spec=spec, start_method=config.start_method, log_dir=config.log_dir, events=ice_events
+    )
+
+    shutdown_rdzv = True
+    try:
+        metrics.initialize_metrics(metrics.MetricsConfig(config.metrics_cfg))
+
+        result = agent.run()
+        # records that agent.run() has succeeded NOT that workers have succeeded
+        events.record(agent.get_event_succeeded())
+
+        if result.is_failed():
+            # ChildFailedError is treated specially by @record
+            # if the error files for the failed children exist
+            # @record will copy the first error (root cause)
+            # to the error file of the launcher process.
+            raise ChildFailedError(
+                name=entrypoint_name,
+                failures=result.failures,
+            )
+
+        return result.return_values
+    except ChildFailedError:
+        raise
+    except SignalException:
+        # when the agent dies with a signal do NOT shutdown the rdzv_handler
+        # since this closes the rendezvous on this rdzv_id permanently and
+        # prevents any additional scaling events
+        shutdown_rdzv = False
+        events.record(agent.get_event_failed())
+        raise
+    except Exception:
+        events.record(agent.get_event_failed())
+        raise
+    finally:
+        if shutdown_rdzv:
+            spec.rdzv_handler.shutdown()
+
+
+if torch.__version__ < "1.11.0":
+    launch_agent = launch_agent_1_10
+else:
+    launch_agent = launch_agent_1_11
--- ice/llutil/multiprocessing/reductions_1_11.py
+++ ice/llutil/multiprocessing/reductions_1_11.py
@@ -100,14 +100,30 @@ def rebuild_tensor(cls, storage, metadata):
     return t
 
 
-def rebuild_cuda_tensor(tensor_cls, tensor_size, tensor_stride, tensor_offset,
-                        storage_cls, dtype, storage_device, storage_handle, storage_size_bytes, storage_offset_bytes,
-                        requires_grad, ref_counter_handle, ref_counter_offset, event_handle, event_sync_required):
+def rebuild_cuda_tensor(
+    tensor_cls,
+    tensor_size,
+    tensor_stride,
+    tensor_offset,
+    storage_cls,
+    dtype,
+    storage_device,
+    storage_handle,
+    storage_size_bytes,
+    storage_offset_bytes,
+    requires_grad,
+    ref_counter_handle,
+    ref_counter_offset,
+    event_handle,
+    event_sync_required,
+):
     # If storage_handle is None, storage points to nullptr.
     if storage_handle is None or storage_size_bytes == 0:
         storage = storage_cls(0, dtype=dtype, device=storage_device)
     else:
-        storage = storage_from_cache(storage_cls, (storage_handle, storage_offset_bytes))
+        storage = storage_from_cache(
+            storage_cls, (storage_handle, storage_offset_bytes)
+        )
         if storage is None:
             torch.cuda._lazy_init()
             storage = storage_cls._new_shared_cuda(
@@ -118,15 +134,23 @@ def rebuild_cuda_tensor(tensor_cls, tensor_size, tensor_stride, tensor_offset,
                 ref_counter_handle,
                 ref_counter_offset,
                 event_handle,
-                event_sync_required)
-            shared_cache[(storage_handle, storage_offset_bytes)] = StorageWeakRef(storage)
+                event_sync_required,
+            )
+            shared_cache[(storage_handle, storage_offset_bytes)] = StorageWeakRef(
+                storage
+            )
         else:
             # We already ref counting this Storage, but producer needs new ref-counters to be released.
-            storage_cls._release_ipc_counter(ref_counter_handle, ref_counter_offset, device=storage_device)
+            storage_cls._release_ipc_counter(
+                ref_counter_handle, ref_counter_offset, device=storage_device
+            )
 
     t = torch._utils._rebuild_tensor(
         torch.storage._TypedStorage(wrap_storage=storage._untyped(), dtype=dtype),
-        tensor_offset, tensor_size, tensor_stride)
+        tensor_offset,
+        tensor_size,
+        tensor_stride,
+    )
 
     if tensor_cls == torch.nn.parameter.Parameter:
         # It is crucial for integer tensors to receive
@@ -142,10 +166,12 @@ def reduce_tensor(tensor):
     storage = tensor.storage()
 
     if tensor.requires_grad and not tensor.is_leaf:
-        raise RuntimeError("Cowardly refusing to serialize non-leaf tensor which requires_grad, "
-                           "since autograd does not support crossing process boundaries.  "
-                           "If you just want to transfer the data, call detach() on the tensor "
-                           "before serializing (e.g., putting it on the queue).")
+        raise RuntimeError(
+            "Cowardly refusing to serialize non-leaf tensor which requires_grad, "
+            "since autograd does not support crossing process boundaries.  "
+            "If you just want to transfer the data, call detach() on the tensor "
+            "before serializing (e.g., putting it on the queue)."
+        )
 
     check_serializing_named_tensor(tensor)
     torch.utils.hooks.warn_if_has_hooks(tensor)
@@ -241,41 +267,49 @@ def reduce_tensor(tensor):
     # thing.
     #
     if storage.is_cuda:
-        (device,
-         handle,
-         storage_size_bytes,
-         storage_offset_bytes,
-         ref_counter_handle,
-         ref_counter_offset,
-         event_handle,
-         event_sync_required) = storage._share_cuda_()
+        (
+            device,
+            handle,
+            storage_size_bytes,
+            storage_offset_bytes,
+            ref_counter_handle,
+            ref_counter_offset,
+            event_handle,
+            event_sync_required,
+        ) = storage._share_cuda_()
         tensor_offset = tensor.storage_offset()
         shared_cache[handle] = StorageWeakRef(storage)
         # _backward_hooks purposely omitted here, see
         # Note [Don't serialize hooks]
-        return (rebuild_cuda_tensor,
-                (type(tensor),
-                 tensor.size(),
-                 tensor.stride(),
-                 tensor_offset,  # tensor offset in its storage
-                 type(storage),
-                 tensor.dtype,
-                 device,
-                 handle,  # identifier which CUDA allocation is the storage in.
-                 storage_size_bytes,  # size(in bytes) of the storage
-                 storage_offset_bytes,  # offset(in bytes) of the storage in the CUDA allocation
-                 tensor.requires_grad,
-                 ref_counter_handle,
-                 ref_counter_offset,
-                 event_handle,
-                 event_sync_required))
+        return (
+            rebuild_cuda_tensor,
+            (
+                type(tensor),
+                tensor.size(),
+                tensor.stride(),
+                tensor_offset,  # tensor offset in its storage
+                type(storage),
+                tensor.dtype,
+                device,
+                handle,  # identifier which CUDA allocation is the storage in.
+                storage_size_bytes,  # size(in bytes) of the storage
+                storage_offset_bytes,  # offset(in bytes) of the storage in the CUDA allocation
+                tensor.requires_grad,
+                ref_counter_handle,
+                ref_counter_offset,
+                event_handle,
+                event_sync_required,
+            ),
+        )
 
     # _backward_hooks purposely omitted here, see Note [Don't serialize hooks]
-    metadata = (tensor.storage_offset(), tensor.size(), tensor.stride(), tensor.requires_grad)
-    return (rebuild_tensor, (
-        type(tensor),
-        storage,
-        metadata))
+    metadata = (
+        tensor.storage_offset(),
+        tensor.size(),
+        tensor.stride(),
+        tensor.requires_grad,
+    )
+    return (rebuild_tensor, (type(tensor), storage, metadata))
 
 
 def fd_id(fd):
@@ -307,17 +341,19 @@ def rebuild_storage_fd(cls, df, size):
 
 
 def rebuild_storage_filename(cls, manager, handle, size, dtype=None):
-    storage: Union[torch._TypedStorage, torch._UntypedStorage] = storage_from_cache(cls, handle)
+    storage: Union[torch._TypedStorage, torch._UntypedStorage] = storage_from_cache(
+        cls, handle
+    )
     if storage is not None:
         return storage._shared_decref()
     if dtype is None:
         storage = torch._UntypedStorage._new_shared_filename(manager, handle, size)
     else:
         byte_size = size * torch._utils._element_size(dtype)
-        untyped_storage: torch._UntypedStorage = torch._UntypedStorage._new_shared_filename(manager, handle, byte_size)
-        storage = torch._TypedStorage(
-            wrap_storage=untyped_storage,
-            dtype=dtype)
+        untyped_storage: torch._UntypedStorage = (
+            torch._UntypedStorage._new_shared_filename(manager, handle, byte_size)
+        )
+        storage = torch._TypedStorage(wrap_storage=untyped_storage, dtype=dtype)
     shared_cache[handle] = StorageWeakRef(storage)
     return storage._shared_decref()
 
@@ -325,25 +361,33 @@ def rebuild_storage_filename(cls, manager, handle, size, dtype=None):
 def rebuild_storage_empty(cls):
     return cls()
 
+
 def rebuild_typed_storage(storage, dtype):
     return torch.storage._TypedStorage(wrap_storage=storage, dtype=dtype)
+
 
 # Use for torch.storage._TypedStorage
 def reduce_typed_storage(storage):
     return (rebuild_typed_storage, (storage._storage, storage.dtype))
 
+
 def rebuild_typed_storage_child(storage, storage_type):
     return storage_type(wrap_storage=storage)
+
 
 # Use for child classes of torch.storage._TypedStorage, like torch.FloatStorage
 def reduce_typed_storage_child(storage):
     return (rebuild_typed_storage_child, (storage._storage, type(storage)))
 
+
 def reduce_storage(storage):
     from . import get_sharing_strategy
+
     if storage.is_cuda:
-        raise RuntimeError("Cannot pickle CUDA storage; try pickling a CUDA tensor instead")
-    elif get_sharing_strategy() == 'file_system':
+        raise RuntimeError(
+            "Cannot pickle CUDA storage; try pickling a CUDA tensor instead"
+        )
+    elif get_sharing_strategy() == "file_system":
         metadata = storage._share_filename_()
         cache_key = metadata[1]
         rebuild = rebuild_storage_filename
@@ -369,7 +413,7 @@ def init_reductions():
     ForkingPickler.register(torch.cuda.Event, reduce_event)
 
     for t in torch._storage_classes:
-        if t.__name__ == '_UntypedStorage':
+        if t.__name__ == "_UntypedStorage":
             ForkingPickler.register(t, reduce_storage)
         else:
             ForkingPickler.register(t, reduce_typed_storage_child)
